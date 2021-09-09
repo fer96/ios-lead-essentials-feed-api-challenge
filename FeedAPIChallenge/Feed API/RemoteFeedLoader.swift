1743915b646166e210303fd3a7819fec667a3692
fer96 <fer96.dlrs@gmail.com>
2021-09-08 21:46:46 -0500

    Create a FeedImageMapper helper to resolves the data and the http response on success case

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,15 +19,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { [weak self] response in
+		client.get(from: url) { response in
 			switch response {
 			case let .success((data, httpResponse)):
-				let items = self?.map(data)
-				if httpResponse.statusCode != 200 || items == nil {
-					completion(.failure(RemoteFeedLoader.Error.invalidData))
-				} else {
-					completion(.failure(RemoteFeedLoader.Error.connectivity))
-				}
+				completion(FeedImageMapper.resolves(data, with: httpResponse))
 			case .failure:
 				completion(.failure(RemoteFeedLoader.Error.connectivity))
 			}
@@ -35,8 +30,36 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 }
 
-extension RemoteFeedLoader {
-	private func map(_ data: Data) -> [FeedImage]? {
-		return nil
+private final class FeedImageMapper {
+	private struct Root: Decodable {
+		private let images: [Image]
+
+		internal var feedImages: [FeedImage] {
+			return images.compactMap { $0.item }
+		}
+	}
+
+	private struct Image: Decodable {
+		private let id: UUID
+		private let description: String?
+		private let location: String?
+		private let url: URL
+
+		internal var item: FeedImage {
+			return FeedImage(id: id, description: description, location: location, url: url)
+		}
+	}
+
+	internal static func resolves(_ data: Data, with httpResponse: HTTPURLResponse) -> FeedLoader.Result {
+		let failureResult = FeedLoader.Result.failure(RemoteFeedLoader.Error.invalidData)
+		guard httpResponse.statusCode == 200 else { return failureResult }
+
+		let feedImages = map(data)
+		return feedImages.isEmpty ? failureResult : .success(feedImages)
+	}
+
+	private static func map(_ data: Data) -> [FeedImage] {
+		guard let root = try? JSONDecoder().decode(Root.self, from: data) else { return [FeedImage]() }
+		return root.feedImages
 	}
 }
